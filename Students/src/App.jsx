33124c0f2ec0dfbb1e2dbbe285725cadd1af05b6
tranducson229 <tranducson2134@gmail.com>
2025-11-12 23:59:50 +0700

    Students

---
+++ Students/src/App.jsx
@@ -0,0 +1,54 @@
+import 'index.css';
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+{students.map(student => (
+          <tr>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))}
+      root .render(
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      );
